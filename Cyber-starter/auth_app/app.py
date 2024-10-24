9b480725885ad580dfdcb054459f06e490020f77
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-24 13:45:43 +0200

    Adding an example auth session

---
+++ Cyber-starter/auth_app/app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.secret_key = 'secretkey'
+
+users = {
+        'testuser': generate_password_hash("password123")
+        }
+
+@app.route('/')
+def home():
+    if 'username' in session:
+        return render_template('home.html', user=session['username'])
+    else:
+        return redirect(url_for('login'))
+
+@pp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username is users and check_password_hash(users[username], password):
+            session['username'] = username
+            flsh('Login successful!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid username or password', 'danger')
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    flash('You have been logged out', 'info')
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
